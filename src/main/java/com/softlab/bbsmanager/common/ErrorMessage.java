4c38b5c29befed7a79182681761970bd0bfbaeae
BogdanXin <499295338@qq.com>
2020-02-22 16:50:05 +0800

    添加一个错误信息类，将处理的错误信息整理起来

---
+++ src/main/java/com/softlab/bbsmanager/common/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.softlab.bbsmanager.common;
+
+/**
+ * @ClassName ErrorMessage
+ * @Description 错误信息类，提供一般出现的错误信息
+ * @Author gwx
+ * @Date 2020/2/22 16:26
+ * @Version 1.0
+ */
+public class ErrorMessage {
+    public static String LOGIN_FALSE = "登录失败！";
+    public static String PARAMETER_ERROR = "请输入用户名和密码！";
+    public static String OPERATION_DENIED = "用户未授权！";
+}
